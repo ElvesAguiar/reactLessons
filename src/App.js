a8f8c25aad37509d74b0987f2514ebfb61425e5d
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-05 21:31:30 -0300

    feat: props lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
 
+const Bemvindo = (props) => {
+ return(
+  <div>
+    
+    <h2>Welcome {props.nome}</h2>
+    <h3>I am {props.idade} years old</h3>
+  
+  </div>
+ 
+ );
+}
+
+
 export default function App(){
   return(
     <div>
       <h1>hello world</h1>
+      <br>
+      </br>
+      <Bemvindo nome="Elves" idade="24"/>
+      <Bemvindo nome = "Vih0" idade="23"/>
+      <Bemvindo nome = "Matias" idade="21"/>
+      
+      <br>
+      </br>
+      
       <h2>@deus</h2>
     </div>
   );
