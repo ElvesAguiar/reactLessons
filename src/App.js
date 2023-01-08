35ae5fa02258eaca9a03fea0441c726f312be9e8
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-07 21:41:51 -0300

    refactor: conditional render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
-import Member from "./components/Member";
 
-class App extends Component {
+class Member extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true,
+    };
+    this.logOut = this.logOut.bind(this);
+    this.logIn = this.logIn.bind(this);
+  }
+
+  logOut(){
+    this.setState({status:false});
+  }
+
+  logIn(){
+    this.setState({status:true})
+  }
+
   render() {
     return (
-    <div>
-      <Member nome="visitante"/>
-    </div>
+      <div>
+        {this.state.status ? (
+          <div>
+            <h2> welcome</h2>
+            <button onClick={this.logOut} >logOut</button>
+          </div>
+        ) : (
+          <div>
+            <h2>ola visitante, faça login!</h2>
+            <button onClick={this.logIn}>Login</button>
+          </div>
+        )}
+      </div>
     );
   }
 }
 
-export default App;
+export default Member;
