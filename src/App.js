f8e7c15bbbe547fb2e785a793c8e0747c9f6d7c7
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-08 20:27:15 -0300

    refactor: forms lesson

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
 import React, { Component } from "react";
-import Feed from "./components/feed";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: [
-        {
-          id: 1,
-          userName: "Marias",
-          like: 10,
-          coments: 2,
-        },
-        {
-          id: 2,
-          userName: "Karol",
-          like: 120,
-          coments: 200,
-        },
-        {
-          id: 3,
-          userName: "Vih0",
-          like: 30,
-          coments: 12,
-        },
-        {
-          id: 4,
-          userName: "teste",
-          like: 1,
-          coments: 1,
-        },
-      ],
+      email:'',
+      password:'',
+      gender:'masculino'
+      
     };
+    this.emailChange = this.emailChange.bind(this)
+  }
+  emailChange(event){
+
+    let input = event.target.value;
+    this.setState({email: input});
+
   }
 
   render() {
     return (
       <div>
-        {this.state.feed.map((item) => {
-          return (
-            <Feed
-              id={item.id}
-              userName={item.userName}
-              like={item.like}
-              coments={item.coments}
-            />
-          );
-        })}
+        <h2>Login</h2>
+        Emai:
+        <input type="email" name="email" value={this.state.email}
+        
+              onChange={this.emailChange} /><br/>
+        Senha:
+        <input type="password" name="senha" value={this.state.password}
+        onChange={(event) => this.setState({password: event.target.value})}/>
+          <br/>
+          <br/>
+        sexo:
+        <select name="sexo" value = {this.state.gender} 
+        onChange={(event) => this.setState({gender: event.target.value})}>
+            <option>Masculino</option>
+            <option>Femenino</option>
+        </select>
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.password}</h3>
+          <h3>{this.state.gender}</h3>
+        </div>
       </div>
     );
   }
